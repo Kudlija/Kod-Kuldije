9e806c20efc1217fee3f73337b2ac8256cc8785d
Kudlija <tarik.kuldija@gmail.com>
2022-12-09 20:28:32 +0100

    zadaca js

---
+++ js/praksa.js
@@ -0,0 +1,35 @@
+//Kontrola toka
+//Comparison operator ili 
+//Petlej
+
+
+//1.
+
+//promt nudi da uneses neku vrijednost
+//alert
+
+
+//console.log('neko klikno na teeee');
+//document.write("novi sadrzaj u html");
+//document.getElementById('centimetraza');
+//let valueOfInput = input.value;
+//console.log("centimetraza" + valueOfInput);
+//if(isNaN(valueOfInput)){
+    //alert("polahko momak");
+
+//}else{
+    //let message = valueOfInput<18?"Premlad si da glassas...": "mozes glasati"
+    //2 omoguciti korisnuku da unese vrijme u sate ako vrijeme bude vece od 18 ispisati korisniku poruku vece u suprotnom dobar dan
+
+
+    function time(){
+      let unos =  prompt ("Unesi vrijeme");
+      console.log(unos);
+    }
+
+
+//ZADACA
+
+//1.pravimo program koji ce dopustiti nasem korisniku da unese naziv dana u sedmici ali na engleskom.
+//u slucaju da postoji taj dan u sedmici korisniku treba da se prikaze da je ispravno unio dan u sedmici i da mu se kaze koji je to dan na bosanskom jeziku.
+//u slucaju da korisnik nije ispravno unio onda treba da mu se prikaze poruka da dan u sedmici nije ispravno unesen.
