b10bf3bd2f493c189600bf4ff0ff38ac963b13ef
Kudlija <tarik.kuldija@gmail.com>
2023-02-10 20:28:23 +0100

    brojac

--- js/neznam.js
+++ js/neznam.js
@@ -1,17 +1,44 @@
-let firstName = 'Tarik ';
-let lastName = 'Kudliaj';
-let fullName= firstName + lastName;
-console.log(fullName);
+let boxId = document.getElementById("box");
+
+if(boxId){
+    boxId.innerHTML = "neki tekst";
+    boxId.style.color = "white"
+    boxId.style.border = "1px solid White"
+}else{
+    console.log('zao mi je fulio si nesto')
+}
+
+let body = document.getElementById('body');
+
+if(body){
+    body.style.backgroundColor = "#000000"
+}else{
+    console.log('zao mi je nesto si fulio nije crno')
+}
+
+const anotherBox = document.createElement("p");
+const box = document.getElementById("box")
+
+if(box){
+    anotherBox.innerHTML = "dobar dan radio sarajevo specijalan pozz za anela kovaceviaca najjaci covjeka na svijetu ljudi ga zovu queen erizzabet"
+    box.append(anotherBox);
+}else{
+    console.log('fulio is')
+}
+
+const dugme = getElementById('button');
+
+function click(){
+    return Function() 
+        console.log('dobri den')
+    }
+    
 
 
-let broj = 10;
-let comparisonRezult = broj == 10;
-console.log(comparisonRezult);
-let text = '10 ';
-comparisonRezult = broj == text;
-console.log(comparisonRezult);
+if(dugme){
+    dugme.onClick = klikni();
 
-let var1 = 10;
-let var2 = '10';
-comparisonRezult = var1 != '10';
-console.log(comparisonRezult);
+    
+}
+
+
